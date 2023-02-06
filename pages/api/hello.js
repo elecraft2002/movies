6469c4a41b73e290c1b329b65b13bc309f6e8f07
Vojtech Suchanek <suchavo3@cvut.cz>
2023-02-06 17:17:58 +0100

    api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,23 +5,25 @@ const api = new ChatGPTAPI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const findMovie = async (sellected, excluded) => {
-  const { text: movie } = await api.sendMessage(`What is a compromise popular movie from these movies: ${sellected.join(
+  return await api.sendMessage(`What is a compromise popular movie from these movies: ${sellected.join(
     ","
   )}. ${excluded && `That is not ${excluded.join(",")}`}
   On first line, tell me name of that movie.
   On second line, send mi a IMDB link, to that movie.
-  On third line, tell me a good reason, why should I watch it.`);
-  movie.split("\n")
+  Nothing more.
+  `);
+  movie.split("\n");
+  return movie
   return { name: movie[0], url: movie[1], text: movie[2] }
 };
 
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    const movie = await findMovie(["Harry Pother", "Avengers"]);
-
-    console.log(movie)
-    res.status(200).json(movie);
+    const { text: movie } = await findMovie(["Harry Pother", "Avengers"]);
+    
+    console.log(movie.split("\n"))
+    res.status(200).json(movie.split("\n"));
   }
   // console.log("Ahoj")
   // res.status(200).json({ name: 'John Doe' })
